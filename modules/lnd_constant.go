47d6658260167f613208b9d266b71157bc1eacf7
Neo Carmack <neo.carmack@lunarion.com>
2019-07-19 20:41:53 +0800

    Update lnd_constant.go

--- modules/lnd_constant.go
+++ modules/lnd_constant.go
@@ -5,12 +5,17 @@ import (
 	"math"
 )
 
+//
 const (
+	 
+	//This value Init_node_chain_hash is the first Mastercoins were generated during the month of August 2013,
+	//and its hash is the parameter [chain_hash:chain_hash] used in open_channel message. 
+	 
 	Init_node_chain_hash = "1EXoDusjGwvnjZUyKkxZ4UHEf77z6A5S4P"
 	maxBtcPaymentMSat    = lnwire.MilliSatoshi(math.MaxUint32)
 )
 
-//数据库
+//database 
 const (
 	DBname     = "lndserver.db"
 	Userbucket = "user"
