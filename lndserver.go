453cfc3b974a93ff456e8be8634a4a64dd241a0e
ZhuGuojun <254698748@qq.com>
2019-07-21 10:57:17 +0800

    support http

--- lndserver.go
+++ lndserver.go
@@ -1,41 +1,18 @@
 package main
 
 import (
+	"LightningOnOmni/routers"
 	"LightningOnOmni/service"
 	"fmt"
-	"github.com/gorilla/websocket"
-	"github.com/satori/go.uuid"
 	"log"
 	"net/http"
 )
-
-func wsPage(res http.ResponseWriter, req *http.Request) {
-	conn, error := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(res, req, nil)
-	if error != nil {
-		http.NotFound(res, req)
-		return
-	}
-	uuid_str, _ := uuid.NewV4()
-	client := &service.Client{
-		Id:           uuid_str.String(),
-		Socket:       conn,
-		Send_channel: make(chan []byte)}
-
-	service.Global_manager.Register <- client
-	go client.Write()
-	client.Read()
-}
 
 func main() {
 	service.Global_params.Interval = 1000
 	service.Global_params.MaximumClients = 1024 * 1024
 	service.Global_params.PoolSize = 4 * 1024
-
 	fmt.Println("Starting application...")
 	go service.Global_manager.Start()
-
-	fmt.Println("Global client manager started...")
-	http.HandleFunc("/ws", wsPage)
-
-	log.Fatal(http.ListenAndServe(":60020", nil))
+	log.Fatal(http.ListenAndServe(":60020", routers.InitRouter()))
 }
---
+++ routers/router.go
@@ -0,0 +1,44 @@
+package routers
+
+import (
+	"LightningOnOmni/service"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"github.com/satori/go.uuid"
+	"net/http"
+)
+
+func InitRouter() *mux.Router {
+	m := mux.NewRouter()
+	m.HandleFunc("/ws", clientConnect)
+	m.HandleFunc("/test", GetTags).Methods("GET")
+	return m
+}
+
+func clientConnect(res http.ResponseWriter, req *http.Request) {
+	conn, error := (&websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}).Upgrade(res, req, nil)
+	if error != nil {
+		http.NotFound(res, req)
+		return
+	}
+	uuid_str, _ := uuid.NewV4()
+	client := &service.Client{
+		Id:           uuid_str.String(),
+		Socket:       conn,
+		Send_channel: make(chan []byte)}
+
+	service.Global_manager.Register <- client
+	go client.Write()
+	client.Read()
+}
+
+func GetTags(writer http.ResponseWriter, request *http.Request) {
+	bytes, err := json.Marshal(&service.User{Id: "1", Email: "123@qq.com"})
+	if err != nil {
+		fmt.Fprintf(writer, "wrong data")
+		return
+	}
+	fmt.Fprintf(writer, string(bytes))
+}
