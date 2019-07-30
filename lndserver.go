6123d875d35e2997cf0df18d846d9e7a6d421fb1
254698748@qq.com <zhuer1982>
2019-07-30 14:45:37 +0800

    rpc package for btc

--- lndserver.go
+++ lndserver.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"LightningOnOmni/routers"
+	"fmt"
+	"github.com/btcsuite/btcd/rpcclient"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+
+	rpcClient()
+
 	routersInit := routers.InitRouter()
 	server := &http.Server{
 		Addr:           ":60020",
@@ -17,4 +22,32 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	log.Fatal(server.ListenAndServe())
+}
+
+func rpcClient() *rpcclient.Client {
+	connCfg := &rpcclient.ConnConfig{
+		Host:         "62.234.188.160:18332",
+		User:         "uprets",
+		Pass:         "pass",
+		HTTPPostMode: true, // Bitcoin core only supports HTTP POST mode
+		DisableTLS:   true, // Bitcoin core does not provide TLS by default
+	}
+	client, err := rpcclient.New(connCfg, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Shutdown()
+
+	client.GetNewAddress("ab")
+	result, err := client.GetInfo()
+	fmt.Println(result)
+
+	// Get the current block count.
+	blockCount, err := client.GetBlockCount()
+	if err != nil {
+		fmt.Println(err)
+	}
+	log.Printf("Block count: %d", blockCount)
+
+	return client
 }
