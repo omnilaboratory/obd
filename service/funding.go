4d1439102972fd328a2e0a8cd294131aa57be1f5
ZhuGuojun <254698748@qq.com>
2019-08-13 15:56:21 +0800

    struct name

--- service/funding.go
+++ service/funding.go
@@ -12,19 +12,19 @@ type FundingManager struct {
 
 var FundingService FundingManager
 
-func (service *FundingManager) CreateFunding(jsonData string) (node *bean.Funding_created, err error) {
+func (service *FundingManager) CreateFunding(jsonData string) (node *bean.FundingCreated, err error) {
 	db, e := dao.DB_Manager.GetDB()
 	if e != nil {
 		return nil, e
 	}
 	tempId, _ := ChannelService.getTemporaryChannelId()
 	hashes, _ := chainhash.NewHashFromStr(gjson.Get(jsonData, "funderPubKey").String())
-	node = &bean.Funding_created{
-		Temporary_channel_id: *tempId,
-		Funder_pubKey:        *hashes,
-		Property_id:          gjson.Get(jsonData, "propertyId").Int(),
-		Max_assets:           gjson.Get(jsonData, "maxAssets").Float(),
-		Amount_a:             gjson.Get(jsonData, "amountA").Float(),
+	node = &bean.FundingCreated{
+		TemporaryChannelId: *tempId,
+		FunderPubkey:       *hashes,
+		PropertyId:         gjson.Get(jsonData, "propertyId").Int(),
+		MaxAssets:          gjson.Get(jsonData, "maxAssets").Float(),
+		AmountA:            gjson.Get(jsonData, "amountA").Float(),
 	}
 
 	return node, db.Save(node)
