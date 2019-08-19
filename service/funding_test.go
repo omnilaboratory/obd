d056386875f51191e367cbdebd0ba0511fb9a933
254698748@qq.com <zhuer1982>
2019-08-19 14:12:00 +0800

    CommitmentTx crud

--- service/funding_test.go
+++ service/funding_test.go
@@ -1,13 +1,14 @@
 package service
 
 import (
+	"LightningOnOmni/bean"
 	"log"
 	"testing"
 )
 
 func TestFundingManager_NextTemporaryChanID(t *testing.T) {
 	for i := 0; i < 10; i++ {
-		tempId := FundingCreateService.NextTemporaryChanID()
+		tempId := bean.ChannelIdService.NextTemporaryChanID()
 		log.Println(tempId)
 		log.Println(string(tempId[:]))
 	}
