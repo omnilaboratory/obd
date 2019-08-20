0d19161caec1487f44502ef355d3eb76443a62a1
254698748@qq.com <zhuer1982>
2019-08-20 12:48:17 +0800

    grpc framework

---
+++ grpcpack/grpcserver.go
@@ -0,0 +1,33 @@
+package grpcpack
+
+import (
+	pb "LightningOnOmni/grpcpack/pb"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	return &pb.HelloReply{Message: "Hello " + in.Name + " age " + in.Age}, nil
+}
+
+func Server() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Println("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Println("failed to serve: %v", err)
+	}
+}
