a900c89f012e4f06207fd4afc74e22aa390c1a13
254698748@qq.com <zhuer1982>
2019-08-22 17:13:09 +0800

    TestBtcRpcManager_CreateMultiSig

--- grpcpack/grpc_test.go
+++ grpcpack/grpc_test.go
@@ -26,3 +26,20 @@ func TestBtcRpcManager_GetNewAddress(t *testing.T) {
 	}
 	log.Println(reply)
 }
+func TestBtcRpcManager_CreateMultiSig(t *testing.T) {
+	conn, err := grpc.Dial("localhost:"+strconv.Itoa(config.GrpcPort), grpc.WithInsecure())
+	defer conn.Close()
+	if err != nil {
+		log.Fatal("Connection error: ", err)
+	}
+	client := pb.NewBtcServiceClient(conn)
+	var keys = make([]string, 2)
+	keys[0] = "n1Grf4JGHUC2CdHHoDRYb7jbVKU2Fv8Tsn"
+	keys[1] = "n4bJvpVHks3Fz9wWB9f445LGV5xTS6LGpA"
+	reply, err := client.CreateMultiSig(context.Background(), &pb.CreateMultiSigRequest{MinSignNum: 1, Keys: keys})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(reply)
+}
