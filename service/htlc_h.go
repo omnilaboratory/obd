45830c6d81089e86cc8d5cee58ec0aa1256fcaef
254698748@qq.com <zhuer1982>
2019-10-14 17:41:33 +0800

    htlc h message

--- service/htlc_h.go
+++ service/htlc_h.go
@@ -6,7 +6,10 @@ import (
 	"LightningOnOmni/tool"
 	"encoding/json"
 	"errors"
+	"github.com/asdine/storm/q"
+	"log"
 	"sync"
+	"time"
 )
 
 type htlcHMessageManager struct {
@@ -25,10 +28,44 @@ func (service *htlcHMessageManager) DealHtlcRequest(jsonData string, creator *be
 		return nil, err
 	}
 
+	tx, err := db.Begin(true)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer tx.Rollback()
+	createRandHInfo := &dao.HtlcCreateRandHInfo{}
+	createRandHInfo.SenderPeerId = creator.PeerId
+	createRandHInfo.RecipientPeerId = htlcHRequest.RecipientPeerId
+	createRandHInfo.PropertyId = htlcHRequest.PropertyId
+	createRandHInfo.Amount = htlcHRequest.Amount
+	createRandHInfo.CurrState = dao.NS_Create
+	createRandHInfo.CreateAt = time.Now()
+	createRandHInfo.CreateBy = creator.PeerId
+	err = tx.Save(createRandHInfo)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	bytes, err := json.Marshal(createRandHInfo)
+	msgHash := tool.SignMsg(bytes)
+	createRandHInfo.RequestHash = msgHash
+	err = tx.Update(createRandHInfo)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
 	data = &bean.HtlcHRespond{}
 	data.PropertyId = htlcHRequest.PropertyId
 	data.Amount = htlcHRequest.Amount
-	data.RequestHash = ""
+	data.RequestHash = msgHash
+
 	return data, nil
 }
 func (service *htlcHMessageManager) DealHtlcResponse(jsonData string, creator *bean.User) (data *dao.HtlcCreateRandHInfo, err error) {
@@ -37,6 +74,12 @@ func (service *htlcHMessageManager) DealHtlcResponse(jsonData string, creator *b
 	}
 	htlcHRespond := &bean.HtlcHRespond{}
 	err = json.Unmarshal([]byte(jsonData), htlcHRespond)
+	if err != nil {
+		return nil, err
+	}
+
+	createRandHInfo := dao.HtlcCreateRandHInfo{}
+	err = db.Select(q.Eq("RequestHash", htlcHRespond.RequestHash), q.Eq("CurrState", dao.NS_Create)).First(&createRandHInfo)
 	if err != nil {
 		return nil, err
 	}
