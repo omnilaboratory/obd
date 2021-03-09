df8b249ec02141105b4230c8ddbedeab15503346
obdCoder <coder.zhuxudong@gmail.com>
2021-03-09 09:58:34 +0800

    grpc test

--- proxy/tests/rpcclient_channel_test.go
+++ proxy/tests/rpcclient_channel_test.go
@@ -8,6 +8,24 @@ import (
 	"testing"
 )
 
+func TestOpenChannel(t *testing.T) {
+	client, conn := getClient()
+	defer conn.Close()
+	channelResponse, err := client.OpenChannel(context.Background(), &proxy.OpenChannelRequest{
+		RecipientInfo: &proxy.RecipientNodeInfo{
+			RecipientNodePeerId: "QmZPzUh7Q6PQg6gXB4XheaoZMMhHA9JNeCrJsp3FWjFrAF",
+			RecipientUserPeerId: "a5f24dc5d5414d961bba98c98624b87222da3984b324bcab7cfd7fd63aee33b3"},
+		NodePubkeyString: "023769b549838e48db217c4d2a8bbeb199c5dbf63dfa38649b6bc2bb18261d7454",
+		NodePubkeyIndex:  1,
+		Private:          false,
+	})
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println(channelResponse.TemplateChannelId)
+}
+
 func TestListChannels(t *testing.T) {
 	client, conn := getClient()
 	defer conn.Close()
@@ -44,7 +62,7 @@ func TestGetTransactionsByChannelId(t *testing.T) {
 	defer conn.Close()
 
 	resp, err := client.GetTransactions(context.Background(), &proxy.GetTransactionsRequest{
-		ChannelId: "1973a103c683f6d8d5b015e3ce4927bf38b7295ccb4f27487b3fc478f2a118cc",
+		ChannelId: "ce7d6a2a15093b80bc9ce12d53c49070ff00bc9e522bf3b40d384cf0c5f5fcc3",
 		PageSize:  10,
 		PageIndex: 1,
 	})
--- proxy/tests/rpcclient_funding_test.go
+++ proxy/tests/rpcclient_funding_test.go
@@ -7,36 +7,14 @@ import (
 	"testing"
 )
 
-func TestOpenChannel(t *testing.T) {
-
-	client, conn := getClient()
-	defer conn.Close()
-
-	channelResponse, err := client.OpenChannel(context.Background(), &proxy.OpenChannelRequest{
-		RecipientInfo: &proxy.RecipientNodeInfo{
-			RecipientNodePeerId: "QmZPzUh7Q6PQg6gXB4XheaoZMMhHA9JNeCrJsp3FWjFrAF",
-			RecipientUserPeerId: "a5f24dc5d5414d961bba98c98624b87222da3984b324bcab7cfd7fd63aee33b3"},
-		NodePubkeyString: "023769b549838e48db217c4d2a8bbeb199c5dbf63dfa38649b6bc2bb18261d7454",
-		NodePubkeyIndex:  1,
-		Private:          false,
-	})
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println(channelResponse.TemplateChannelId)
-}
-
 func TestFundChannel(t *testing.T) {
-
 	client, conn := getClient()
 	defer conn.Close()
-
 	fundChannel, err := client.FundChannel(context.Background(), &proxy.FundChannelRequest{
 		RecipientInfo: &proxy.RecipientNodeInfo{
 			RecipientNodePeerId: "QmZPzUh7Q6PQg6gXB4XheaoZMMhHA9JNeCrJsp3FWjFrAF",
 			RecipientUserPeerId: "a5f24dc5d5414d961bba98c98624b87222da3984b324bcab7cfd7fd63aee33b3"},
-		TemplateChannelId: "ac98aef33e317a0063036c33ffd9b3703fd3e630ea22ffc2ba0b05e7da475ab8",
+		TemplateChannelId: "1c4fc0f4a75bf372f37979acc6db1524f6bab689520a913625e533619b67c05c",
 		BtcAmount:         0.0004,
 		PropertyId:        137,
 		AssetAmount:       1,
