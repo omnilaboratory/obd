0cb4a6fe9bd7b6d23ea683d71bb5ede56aa54409
obdCoder <coder.zhuxudong@gmail.com>
2021-03-15 11:37:37 +0800

    grpc SendRequest， SendResponse

--- proxy/rpc/server.go
+++ proxy/rpc/server.go
@@ -4,6 +4,7 @@ import (
 	"github.com/omnilaboratory/obd/config"
 	proxy "github.com/omnilaboratory/obd/proxy/pb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
 	"strconv"
@@ -26,6 +27,7 @@ func StartGrpcServer() {
 	log.Printf("grpc Server is listening on %v ...", address)
 
 	s := grpc.NewServer()
+	reflection.Register(s)
 	proxy.RegisterLightningServer(s, &RpcServer{})
 	proxy.RegisterWalletServer(s, &RpcServer{})
 	proxy.RegisterRsmcServer(s, &RpcServer{})
